16626294b356b42ce60340d56d1cab36bf6cd920
DavidSzczesniak <david.szczesniak86@gmail.com>
2022-01-02 23:31:39 +0000

    Added more content to the homepage
    - added a simple 'features overview' to the homepage below the main content
    - added new illustration to the header, replacing the big hand as that wasn't working in my opinion
    - replaced the svg 'blob' in the background, with a simple separator div which is styled slightly to make it curve from one side.
    - updated button colors
    - updated box container styling so it stands out a bit more on both neon green and white backgrounds

--- pages/index.js
+++ pages/index.js
@@ -20,19 +20,57 @@ export default function Home() {
                     </h1>
                     <div className="homepage__image">
                         <Image
-                            src="/hand-thumbs-up.png"
-                            alt="Hand showing thumbs up"
+                            src="/pixeltrue-money-jar.png"
+                            alt="money jar"
                             width={225}
-                            height={200}
+                            height={350}
                         />
                     </div>
                 </div>
-                <div className="homepage__content">
+                <div className="homepage__tagline">
                     <p>
                         Funny tagline with some text will go here to add some content underneath the
                         big text above.
                     </p>
                     <button className="button button--body">Get Started</button>
+                </div>
+                <div className="homepage__content ">
+                    <div className="app-feature box-container">
+                        <Image
+                            src="/pixeltrue-receipt.png"
+                            alt="receipt"
+                            width={150}
+                            height={120}
+                        />
+                        <div className="app-feature__text">
+                            <h2>Log it</h2>
+                            <p>Log your expenses one by one.</p>
+                        </div>
+                    </div>
+                    <div className="app-feature box-container">
+                        <Image
+                            src="/pixeltrue-track.png"
+                            alt="webpage search"
+                            width={150}
+                            height={120}
+                        />
+                        <div className="app-feature__text">
+                            <h2>Track it</h2>
+                            <p>Search, filter, edit and organise your recorded expenses.</p>
+                        </div>
+                    </div>
+                    <div className="app-feature box-container">
+                        <Image
+                            src="/pixeltrue-magnify.png"
+                            alt="magnifying glass"
+                            width={150}
+                            height={120}
+                        />
+                        <div className="app-feature__text">
+                            <h2>Analyze it</h2>
+                            <p>Analyze your spending habits and improve them.</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
