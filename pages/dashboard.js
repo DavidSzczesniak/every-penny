bed5bea3d90bf2a638ad5c8be73e6c18d0ac5ca8
DavidSzczesniak <david.szczesniak86@gmail.com>
2022-01-22 18:12:08 +0000

    Added 'no expenses' screen to Dashboard page
    - if the user has no expenses in their list and they navigate to the dashboard page, a message will come up advising them to add some

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -3,11 +3,18 @@ import DashboardChart from 'components/DashboardChart';
 import DashboardHeader from 'components/DashboardHeader';
 import DashboardStats from 'components/DashboardStats';
 import PageWrapper from 'components/PageWrapper';
+import { ExpensesContext } from 'context/expensesContext';
 import { useFilters } from 'hooks/useFilters';
 import React, { useEffect, useState } from 'react';
+import { useContext } from 'react/cjs/react.development';
 import { getDateRange } from 'utils/helpers';
+import Image from 'next/image';
+import { Button } from '@mantine/core';
+import { primaryButtonStyles } from 'utils/customButtonStyles';
+import Link from 'next/link';
 
 const Dashboard = () => {
+    const { allExpenses } = useContext(ExpensesContext);
     const { filterByDateRange } = useFilters();
     const [period, setPeriod] = useState('30');
 
@@ -19,12 +26,33 @@ const Dashboard = () => {
     return (
         <PageWrapper title="Dashboard">
             <div className="dashboard">
-                <DashboardHeader period={period} setPeriod={setPeriod} />
-                <main>
-                    <DashboardStats />
-                    <DashboardChart period={period} />
-                    <DashboardAllocation />
-                </main>
+                {allExpenses.length > 0 ? (
+                    <>
+                        <DashboardHeader period={period} setPeriod={setPeriod} />
+                        <main>
+                            <DashboardStats />
+                            <DashboardChart period={period} />
+                            <DashboardAllocation />
+                        </main>
+                    </>
+                ) : (
+                    <div className="info-page">
+                        <Image
+                            src="/pixeltrue-search.png"
+                            alt="magnifying glass"
+                            width={400}
+                            height={325}
+                        />
+                        <h2>Looks like you haven&apos;t added any expenses!</h2>
+                        <p>
+                            Use the button above or navigate to the Expenses List to add some
+                            expenses.
+                        </p>
+                        <Link href="/list" passHref>
+                            <Button styles={primaryButtonStyles}>Go there</Button>
+                        </Link>
+                    </div>
+                )}
             </div>
         </PageWrapper>
     );
