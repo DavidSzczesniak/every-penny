d3783ba7d58f95001b085881c64c1f583c2cb06b
DavidSzczesniak <david.szczesniak86@gmail.com>
2022-01-25 21:41:11 +0000

    Added tests for Dashboard
    - moved the dashboard wrapper to its own component, because it would clash with PageWrapper otherwise and be in the loading state infinitely
    - added some basic tests

---
+++ test/pages/Dashboard.test.js
@@ -0,0 +1,52 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { ExpensesContext } from 'context/expensesContext';
+import Dashboard from 'components/Dashboard';
+import React from 'react';
+import { mockExpenses } from '../../__mocks__/expenses';
+import '../../__mocks__/ResizeObserver';
+
+const mockDispatch = jest.fn();
+const renderComponent = (defaultExpenses = mockExpenses) => {
+    const defaults = {
+        allExpenses: defaultExpenses,
+        textFilter: '',
+        dateRangeFilter: [null, null],
+        categoryFilter: '',
+        dispatch: mockDispatch,
+    };
+    render(
+        <ExpensesContext.Provider value={{ ...defaults }}>
+            <Dashboard />
+        </ExpensesContext.Provider>
+    );
+};
+
+it('should render with basic props', () => {
+    renderComponent();
+    expect(screen.getByRole('heading', { name: 'Total expenses' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Top category' })).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: 'Allocation' })).toBeInTheDocument();
+});
+
+it('should render no data screen', () => {
+    renderComponent([]);
+    expect(screen.getByRole('img', { name: 'magnifying glass' })).toBeInTheDocument();
+    expect(
+        screen.getByRole('heading', { name: "Looks like you haven't added any expenses!" })
+    ).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Go there' })).toBeInTheDocument();
+});
+
+it('should dispatch date filter', async () => {
+    renderComponent();
+
+    userEvent.click(screen.getByRole('textbox', { name: 'Select time period' }));
+    userEvent.click(screen.getByRole('option', { name: 'Week' }));
+    await waitFor(() => {
+        expect(mockDispatch).toHaveBeenCalledWith({
+            type: 'SET_DATE_RANGE_FILTER',
+            filter: expect.any(Array),
+        });
+    });
+});
